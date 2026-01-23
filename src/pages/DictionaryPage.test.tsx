ee72bcd5f6b5da091de3bd67971666664d16083f
Konstantin Yarish <konstantin.yarish@gmail.com>
2026-01-24 00:20:21 +0300

    Исправить замечание Sonar

--- src/pages/DictionaryPage.test.tsx
+++ src/pages/DictionaryPage.test.tsx
@@ -1,9 +1,8 @@
-import { render } from '@/test/test-uitls';
-import { describe, it, vi } from 'vitest';
-import DictionaryPage from './DictionaryPage';
-import { expect } from 'vitest';
-import * as dictionaryApi from '@/api/dictionary.api';
 import { verbsStub } from '@/api/__stubs__/dictionary.stub';
+import * as dictionaryApi from '@/api/dictionary.api';
+import { render } from '@/test/test-uitls';
+import { describe, expect, it, vi } from 'vitest';
+import DictionaryPage from './DictionaryPage';
 
 vi.mock('@/api/dictionary.api');
 
